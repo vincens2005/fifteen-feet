e312e5930a721dd6e600a74cad3a874925cdb8d2
cukmekerb <cukmekerb@gmail.com>
2022-05-06 17:11:07 -0700

    working proxy

---
+++ netlify/edge-functions/proxy.js
@@ -0,0 +1,21 @@
+export default async (request, context) => {
+	let url = request.url.split("/proxy/");
+	url.shift(); // remove first element
+	url = url.join(""); // combine other ones. now we've removes the first instance of /proxy/
+	url = decodeURIComponent(url);
+	
+	if (!url) return new Response("error: no url provided");
+	
+	console.log(url)
+	url = new URL(url);
+	
+	
+	let response = await fetch(url, {
+		headers: {
+			"user-agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"
+		}
+	});
+	
+
+	return response;
+};
