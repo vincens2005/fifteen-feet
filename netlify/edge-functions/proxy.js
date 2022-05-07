e0dfb9efab016aa39f468e4ec145b402a6f1791c
cukmekerb <cukmekerb@gmail.com>
2022-05-06 18:02:36 -0700

    fancy landing page

--- netlify/edge-functions/proxy.js
+++ netlify/edge-functions/proxy.js
@@ -1,7 +1,7 @@
 export default async (request, context) => {
-	let url = request.url.split("/proxy/");
+	let url = request.url.split(/\/proxy(?:\/\?q=|\/\!?|\?q=)/);
 	url.shift(); // remove first element
-	url = url.join(""); // combine other ones. now we've removes the first instance of /proxy/
+	url = url.join(""); // combine other ones. now we've removes the first instance of /proxy/?q=
 	url = decodeURIComponent(url);
 	
 	if (!url) return new Response("error: no url provided");
